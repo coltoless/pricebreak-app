522e306b66bab84e05f3d4d40ca2dd87955881ed
KaterynaTymofeieva <kattym@uw.edu>
2025-05-04 19:10:04 -0700

    Add site lock to redirect all requests to coming soon page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  before_action :redirect_to_coming_soon
+
+  private
+
+  def redirect_to_coming_soon
+    return if controller_name == 'home' && action_name == 'index'
+    return if controller_name == 'notifications' && action_name == 'create'
+    
+    redirect_to root_path
+  end
 end
