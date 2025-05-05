8ef59855306c8c8c379424b75023ee89caf81444
KaterynaTymofeieva <kattym@uw.edu>
2025-05-04 21:35:29 -0700

    Improve email submission UI and feedback

--- app/controllers/launch_subscribers_controller.rb
+++ app/controllers/launch_subscribers_controller.rb
@@ -5,7 +5,13 @@ class LaunchSubscribersController < ApplicationController
     if @subscriber.save
       flash[:success] = "Thanks for your interest! We'll notify you when we launch."
     else
-      flash[:error] = "Sorry, there was an error saving your email."
+      if @subscriber.errors[:email].include?("has already been taken")
+        flash[:error] = "This email is already registered. We'll notify you when we launch!"
+      elsif @subscriber.errors[:email].include?("is invalid")
+        flash[:error] = "Please enter a valid email address."
+      else
+        flash[:error] = "Sorry, there was an error saving your email. Please try again."
+      end
     end
     
     redirect_to root_path
