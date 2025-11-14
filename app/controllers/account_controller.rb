6959cd95d28ed033e01b126b1d1f57e045f3c09d
KaterynaTymofeieva <kattym@uw.edu>
2025-11-13 21:16:39 -0800

    Disable layout for account page to prevent JavaScript from loading and causing redirects

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -1,6 +1,7 @@
 class AccountController < ApplicationController
   # Skip Firebase authentication for HTML views - we'll handle auth client-side
   skip_before_action :redirect_to_coming_soon
+  layout false  # Don't use layout to avoid loading JavaScript that causes redirects
   
   def index
     # Account dashboard - user data will be fetched client-side via API
